02df58d0c2937e4f80fcd47affb1d49af1fa65f2
chenriyi <chenriyi@star-net.cn>
2023-02-27 09:58:50 +0800

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
+import Components from 'unplugin-vue-components/vite';
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
+  },
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [AntDesignVueResolver()]
+    })
+  ]
+});
